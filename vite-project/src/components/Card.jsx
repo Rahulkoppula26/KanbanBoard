7538b1323456107d07b7deab8b2f54a4bff5d388
Rahulkoppula <135603322+Rahulkoppula26@users.noreply.github.com>
2024-12-26 22:04:08 +0530

    Update Card.jsx

--- vite-project/src/components/Card.jsx
+++ vite-project/src/components/Card.jsx
@@ -1,4 +1,4 @@
-import "./Style.css";
+import "./style.css";
 // import { useState } from "react";
 import {Link} from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
